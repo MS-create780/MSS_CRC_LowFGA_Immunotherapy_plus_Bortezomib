0b1a79f09ff1784b9894b1305b7862416fd101b6
MS-create780 <mengyuanshipku@gmail.com>
2026-04-11 11:23:39 +1000

    org_DEG_volcano plot.R

--- Organoids_RNA+WES/org_DEG_volcano plot.R
+++ Organoids_RNA+WES/org_DEG_volcano plot.R
@@ -8,19 +8,6 @@ fig[which(fig$log2FoldChange <= -1 & fig$padj < 0.05),'sig'] <- 'downregulated'
 fig[which(fig$padj %in% NA),'sig'] <- 'no diff'
 fig$sig[which(is.na(fig$sig))] <- 'no diff'
 
-up_top10 <- fig[fig$sig == "upregulated", ]
-up_top10 <- up_top10[order(-up_top10$log2FoldChange), ][1:10, ]
-
-
-down_top10 <- fig[fig$sig == "downregulated", ]
-down_top10 <- down_top10[order(down_top10$log2FoldChange), ][1:10, ]
-
-
-label_genes <- rbind(up_top10, down_top10)
-qq=label_genes$X
-qq=qq[-c(6,18)]
-qq
-colnames(fig)
 cut_off_qvalue = 0.05
 cut_off_logFC = 1
 p=ggplot(fig, aes(x = log2FoldChange, y = -log10(padj), colour=sig)) +
@@ -35,20 +22,6 @@ p=ggplot(fig, aes(x = log2FoldChange, y = -log10(padj), colour=sig)) +
   theme(plot.title = element_text(hjust = 0.5), 
         legend.position="right", 
         legend.title = element_blank() 
-  ) +  
-  geom_text_repel(
-    data = fig[fig$X %in% qq,],
-    aes(label = X), 
-    size = 3,
-    point.padding = unit(1.5, "lines"),  
-    box.padding = unit(1, "lines"),     
-    max.overlaps = 20,  
-    force = 2,           
-    segment.color = "gray50",  
-    segment.size = 0.3,        
-    bg.color = "white",        
-    bg.r = 0.1,                
-    show.legend = FALSE
-  )
+  ) 
 p
-ggsave("./job_example/volcano+FGA LOW MSS VS. CLASSIC MSS.png",p,width=7,height=4)
+ggsave("./job_example/volcano+FGA LOW MSS VS. FGA high MSS.png",p,width=7,height=4)
